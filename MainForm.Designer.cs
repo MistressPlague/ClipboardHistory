1013052534df575812faed4749fb592c06230f62
MistressPlague <krewellamusicps3@gmail.com>
2022-08-25 04:22:56 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Windows.Forms;
+
+namespace ClipboardHistory
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.LayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.ClipboardMonitor = new System.Windows.Forms.Timer(this.components);
+            this.SystemTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TrayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TrayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Location = new System.Drawing.Point(768, 12);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(20, 20);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "X";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // LayoutPanel
+            // 
+            this.LayoutPanel.AutoScroll = true;
+            this.LayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.LayoutPanel.Location = new System.Drawing.Point(12, 37);
+            this.LayoutPanel.Name = "LayoutPanel";
+            this.LayoutPanel.Size = new System.Drawing.Size(776, 401);
+            this.LayoutPanel.TabIndex = 1;
+            this.LayoutPanel.WrapContents = false;
+            // 
+            // ClipboardMonitor
+            // 
+            this.ClipboardMonitor.Enabled = true;
+            this.ClipboardMonitor.Tick += new System.EventHandler(this.ClipboardMonitor_Tick);
+            // 
+            // SystemTrayIcon
+            // 
+            this.SystemTrayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.SystemTrayIcon.BalloonTipText = "Click To Open";
+            this.SystemTrayIcon.BalloonTipTitle = "ClipboardHistory";
+            this.SystemTrayIcon.ContextMenuStrip = this.TrayMenu;
+            this.SystemTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("SystemTrayIcon.Icon")));
+            this.SystemTrayIcon.Text = "ClipboardHistory";
+            this.SystemTrayIcon.Visible = true;
+            this.SystemTrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SystemTrayIcon_MouseDoubleClick);
+            // 
+            // TrayMenu
+            // 
+            this.TrayMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.TrayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.TrayMenu.Name = "TrayMenu";
+            this.TrayMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.TrayMenu.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.showToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ClipboardHistory";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LayoutPanel);
+            this.Controls.Add(this.CloseButton);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "ClipboardHistory";
+            this.TrayMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.FlowLayoutPanel LayoutPanel;
+        private System.Windows.Forms.Timer ClipboardMonitor;
+        private System.Windows.Forms.NotifyIcon SystemTrayIcon;
+        private Label label1;
+        private ContextMenuStrip TrayMenu;
+        private ToolStripMenuItem showToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardHistory
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClipboardMonitor_Tick(object sender, EventArgs e)
+        {
+            var Entry = Clipboard.GetText();
+
+            if (LayoutPanel.Controls.ToList().All(o => o.Text != Entry))
+            {
+                var button = new Button
+                {
+                    Text = Entry,
+                    FlatStyle = FlatStyle.Flat,
+                    FlatAppearance = { BorderColor = Color.FromArgb(50, 50, 50)},
+                    BackColor = Color.FromArgb(17, 17, 17),
+                    ForeColor = Color.FromArgb(153, 153, 153),
+                    Size = new Size(770, 31),
+                    Name = $"CopyButton_{LayoutPanel.Controls.Count + 1}"
+                };
+
+                button.Click += (_, _) =>
+                {
+                    Clipboard.SetText(Entry);
+                };
+
+                LayoutPanel.Controls.Add(button);
+
+                LayoutPanel.ScrollControlIntoView(button);
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowInTaskbar = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void SystemTrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowInTaskbar = true;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+
+    internal static class Extensions
+    {
+        internal static List<Control> ToList(this Control.ControlCollection Col)
+        {
+            var list = new List<Control>();
+
+            foreach (Control control in Col)
+            {
+                list.Add(control);
+            }
+
+            return list;
+        }
+    }
+}
